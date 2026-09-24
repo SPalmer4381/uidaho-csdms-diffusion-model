1e1aa93fe5ebee4738cbede328d5cddf2debc801
Sarah Palmer <spalmer4381@gmail.com>
2026-09-24 10:36:02 -0700

    Package model (#6)
    * Ignore build artifacts and virtual environment
    
    * Add pyproject.toml for packaging
    
    * Make a main program
    
    * Clean lint and reformat main script
    
    * Clean lint and reformat main script
    
    * Put guards on title and outfile keywords

--- test_Diffusion_Model.py
+++ test_Diffusion_Model.py
@@ -1,5 +1,6 @@
-from  Diffusion_Model import calculate_stable_time_step
+from Diffusion_Model import calculate_stable_time_step
+
 
 def test_time_step_is_float():
-        time_step = calculate_stable_time_step(1, 1)
-        assert isinstance(time_step, float)
+    time_step = calculate_stable_time_step(1, 1)
+    assert isinstance(time_step, float)
